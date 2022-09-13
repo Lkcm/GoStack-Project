638b47cdb62273f286b865349fab6be32b6ca617
Lucas Kroger <lucasmkroger12@gmail.com>
2022-09-13 12:23:26 -0300

    atu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
+import api from './services/api'
+
+import './App.css'
 import {Header} from './components/Header'
+// import { response } from 'express';
 
 
 
 
 export function App() {
-  const [projects, setProjects] = useState(["Diamante", "Netherite"]);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(()=> {
+    api.get('/').then(response => {
+      console.log(response.data[0]);
+      setProjects(response.data);
+    });
+  }, []);
 
   // useState retorna um array com 2 posicoes
   //
@@ -24,7 +35,7 @@ return(
     <Header title="Picaretas"/>
 
     <ul>
-      {projects.map(project => <li key={project}>{project}</li>)}
+      {projects.map(project => <li key={project.id}>{project.title}</li>)}
     </ul>
 
     <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
